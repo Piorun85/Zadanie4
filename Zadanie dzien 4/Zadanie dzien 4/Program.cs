0864505f34131e4dd90d6207acbab044727f1fbd
Marcin Cela <celamarcin@gmail.com>
2023-01-29 19:02:47 +0100

    Zadanie dzień 4

---
+++ Zadanie dzien 4/Zadanie dzien 4/Program.cs
@@ -0,0 +1,24 @@
+﻿var name = "Marcin";
+var gender = "man";
+var age = 37;
+
+if (gender == "man" && age > 30)
+{
+    Console.WriteLine("Mężczyzna w wieku powyżej 30");
+}
+else if (gender == "man" && age > 40)
+{
+    Console.WriteLine("Mężczyzna w wieku ponad 40 lat");
+}
+if (name == "Marcin" && age == 37)
+{
+    Console.WriteLine("Marcin lat 37");
+}
+else if (gender == "man" && age < 18)
+{
+    Console.WriteLine("Pełnoletni mężczyzna");
+}
+if (gender == "man" && age > 18) ;
+{
+    Console.WriteLine("Pełnoletni Mężczyzna");
+}
